baed027fe9991edf4eeddfc2edd3405057d1df68
Wollffus <58875783+Wollffus@users.noreply.github.com>
2021-02-20 23:00:19 -0800

    Add files via upload

---
+++ scrp_boats/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.NoMoney = 'Do not have enough money $'
+Config.Shoptext = '(E)Buy a Boat'
+Config.LevelMissing = 'You must be level 1 or higher'
+Config.Coords = {
+	vector3(2841.82,-1406.85,45.39),
+	vector3(340.23,-674.39,42.8),
+}
+
+  
+  
